012b68d9281bf089df67060e77a72fcfb974f822
adrianhajdin <adrianhajdin00@gmail.com>
2020-02-23 15:43:36 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+import Posts from './components/Posts/Posts';
+
+const App = () => (
+    <div className="main-container">
+        <h1 className="main-heading">Breaking WEATHER News</h1>
+        <Posts /> 
+    </div>
+);          
+
+export default App;
